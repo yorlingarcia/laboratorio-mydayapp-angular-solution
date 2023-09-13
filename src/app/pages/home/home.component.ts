df4c7aef03808f38d1f1ed2900c193bacb2f5e0d
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-09-13 17:28:24 -0500

    uso de expresion regular para modificar el valor del input

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Todos } from 'src/app/interfaces/todos.interface';
+import { StorageTodosService } from 'src/app/services/storage-todos.service';
 import { TodosService } from 'src/app/services/todos.service';
 
 @Component({
@@ -7,9 +8,20 @@ import { TodosService } from 'src/app/services/todos.service';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  value = '';
   todoArray: Todos[] = [];
+  miArrayJSON = JSON.stringify(this.todoArray);
 
-  constructor(private todosService: TodosService) {
+  constructor(
+    private todosService: TodosService,
+    private storageService: StorageTodosService
+  ) {
     this.todoArray = this.todoArray;
+    this.storageService.getStorage();
+  }
+
+  onEnter(value: string) {
+    this.value = value;
+    const texto_2 = this.value.replace(/^\s+|\s+$|\s+(?=\s)/g, '');
   }
 }
--- src/app/services/storage-todos.service.ts
+++ src/app/services/storage-todos.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageTodosService {
+  constructor() {}
 
-  constructor() { }
+  getStorage() {
+    if (!localStorage.getItem('mydayapp-angular')) {
+      localStorage.setItem('mydayapp-angular', '');
+      console.log('creacion de almacenamiento');
+    }
+  }
 }
