97b247891953ef843f059a24414a65103e27cbe9
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-09-13 16:31:03 -0500

    validacion de la cantidad de todos iniciales

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { Todos } from 'src/app/interfaces/todos.interface';
+import { TodosService } from 'src/app/services/todos.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  constructor() {}
+  todoArray: Todos[] = [];
+
+  constructor(private todosService: TodosService) {
+    this.todoArray = this.todoArray;
+  }
 }
