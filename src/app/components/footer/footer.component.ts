db27dc7093a4159dd652ba18f0f537779c5cbbea
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-09-16 10:23:33 -0500

    estado del checkbox completado, mejora al ngModel para solo recibir datos

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { StorageTodosService } from 'src/app/services/storage-todos.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  constructor(private storageTodoService: StorageTodosService) {
+    this.conteoDeTodos = this.storageTodoService.todos.length;
+  }
 
+  conteoDeTodos: number = 0;
+
+  get pluralizacionItem() {
+    if (this.conteoDeTodos != 1) {
+      return 'items';
+    } else return 'item';
+  }
+
+  get lengthTodos() {
+    return this.storageTodoService.todos.length;
+  }
 }
